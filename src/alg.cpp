a43668f5e514ea671949d5c06998aaa8cf9e6802
Semen-Chugunov <125124591+Semen-Chugunov@users.noreply.github.com>
2023-02-25 16:09:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    if (a == b) 
-        return a; 
-    if (a > b) 
-        return gcd(a-b,b);
+    if (a == b)
+        return a;
+    if (a > b)
+        return gcd(a-b, b);
     return gcd(a, b-a);
 }
